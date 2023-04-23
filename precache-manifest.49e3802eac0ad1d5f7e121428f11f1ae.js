9f42cc4a5981aaaa44c204c014616de8ab9f6e11
Tung Pham <phamthanh.tung1@heineken.com>
2023-04-23 23:26:10 +0700

    ok

---
+++ precache-manifest.49e3802eac0ad1d5f7e121428f11f1ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa009a47c2db13ba6c1918c27926a6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d82d8aa34aa617df70c",
+    "url": "/static/js/2.b32ff99b.chunk.js"
+  },
+  {
+    "revision": "dda2a51fac4c1fd83655798c7116210f",
+    "url": "/static/js/2.b32ff99b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "367f52cdecdee94e8f9b",
+    "url": "/static/js/main.557db64f.chunk.js"
+  },
+  {
+    "revision": "33261f7c2b17c0f4a91e",
+    "url": "/static/js/runtime-main.488e8d44.js"
+  }
+]);
