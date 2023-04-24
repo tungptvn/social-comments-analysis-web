92adec3a7ac8287df544d05ee1a78b9c46f06599
Tung Pham <phamthanh.tung1@heineken.com>
2023-04-24 20:03:06 +0700

    update footer

--- precache-manifest.e0d6f0288d17aeb4259a8220f309e419.js
+++ precache-manifest.e0d6f0288d17aeb4259a8220f309e419.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9aa009a47c2db13ba6c1918c27926a6c",
+    "revision": "e17d1f3bd78feb8972c5552420c19ecf",
     "url": "/index.html"
   },
   {
-    "revision": "3d82d8aa34aa617df70c",
-    "url": "/static/js/2.b32ff99b.chunk.js"
+    "revision": "ccf2a93dd166d8e456bf",
+    "url": "/static/js/2.901b9394.chunk.js"
   },
   {
     "revision": "dda2a51fac4c1fd83655798c7116210f",
-    "url": "/static/js/2.b32ff99b.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.901b9394.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "367f52cdecdee94e8f9b",
-    "url": "/static/js/main.557db64f.chunk.js"
+    "revision": "35f03b87fb4ef601e1de",
+    "url": "/static/js/main.8b5142d4.chunk.js"
   },
   {
     "revision": "33261f7c2b17c0f4a91e",
